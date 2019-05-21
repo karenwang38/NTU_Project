bfd2b58b2ab061a8bac4ef99ca09f5cc64cdbaef
karen <karen.wang38@gmail.com>
2019-05-21 08:59:48 +0800

    modify index.html, back.py

--- PARK/back.py
+++ PARK/back.py
@@ -1,46 +1,22 @@
 # -*- coding: utf-8 -*-
 from wsgiref.simple_server import make_server
-
-'''
-class MyApplication():
-
-    def __init__(self, environ, start_response):
-        self.start_response = start_response
-        self.path = environ['PATH_INFO']
-        self.request_method = environ['REQUEST_METHOD']
-
-    def __iter__(self):
-        if self.path == '/':
-            status = '200 OK'
-            response_headers = [('Content-type', 'text/plain')]
-            self.start_response(status, response_headers)
-            yield "I'm in the home"
-
-        elif self.path == '/aljun':
-            status = '200 OK'
-            response_headers = [('Content-type', 'text/plain')]
-            self.start_response(status, response_headers)
-            yield "I'm in the aljun's home,it's pretty"
-
-        else:
-            status = '404 NOT FOUND'
-            response_headers = [('Content-type', 'text/plain')]
-            self.start_response(status, response_headers)
-            yield "404 NOT FOUND"
-
-if __name__ == "__main__":
-    app = make_server('127.0.0.1', 8000, MyApplication)
-    app.serve_forever()
-'''
+import json
 from cgi import parse_qs, escape
-
-# html中form的method是get，action是当前页面
 html = """
 <html>
 <body>
    <form method="get" action="">
         <p>
+           ID: <input type="text" name="id" value="%(id)s">
+        </p>
+        <p>
            Name: <input type="text" name="name" value="%(name)s">
+        </p>
+        <p>
+           Location: <input type="text" name="location" value="%(location)s">
+        </p>
+        <p>
+           Price: <input type="text" name="price" value="%(price)s">
         </p>
         <p>
             State:
@@ -58,31 +34,55 @@ html = """
         </p>
     </form>
     <p>
+        ID: %(id)s<br>
         Name: %(name)s<br>
+        Location: %(location)s<br>
+        Price: %(price)s<br>
         State: %(state)s
     </p>
 </body>
 </html>
 """
-park={}
+park=[]
 def application (environ, start_response):
 
     # 解析QUERY_STRING
     d = parse_qs(environ['QUERY_STRING'])
-
+    id = d.get('id', [''])[0] # 返回id對應的值
     name = d.get('name', [''])[0] # 返回name對應的值
+    location = d.get('location', [''])[0] # 返回location對應的值
+    price = d.get('price', [''])[0] # 返回price對應的值
     state = d.get('state', []) # 返回state對應的值
-    if len(state):
-        park[name]=state[0]
-        print park
+    if len(state)==1 and id.isdigit() and name!='' and location!='' and price.isdigit():
+        new_park={}
+        new_park["id"]=id
+        new_park["name"]=name
+        new_park["location"]=location
+        new_park["price"]=price
+        new_park["state"]=state[0]
+        for i in range(len(park)):
+            if park[i]["id"] == id:
+                del park[i]
+        park.append(new_park)
+    
+    json_park = json.dumps(park)
+    with open('data.json', 'w') as f:
+        json.dump(json_park, f)
+    print (park)
     # 防止腳本注入
-    name = escape(name)
+    id = escape(id)
+    name = escape(name)    
+    location = escape(location)
+    price = escape(price)
     state = [escape(st) for st in state]
 
     response_body = html % { 
         'checked-close': ('', 'checked')['close' in state],
         'checked-open': ('', 'checked')['open' in state],
-        'name': name or 'Empty',
+        'id': id or '',
+        'name': name or '',
+        'location': location or '',
+        'price': price or '',
         'state': ', '.join(state or ['No state?'])
     }
 
@@ -101,5 +101,3 @@ httpd = make_server('localhost', 8051, application)
 
 # 一直處理請求
 httpd.serve_forever()
-
-print 'end'
